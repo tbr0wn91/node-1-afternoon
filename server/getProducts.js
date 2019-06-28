40388c49fadc3761e0be7cd98cdcc9e019d333d8
thomas brown <thomasbrown9101@gmail.com>
2019-06-28 14:39:53 -0700

    finished node 1 afternoon

---
+++ server/getProducts.js
@@ -0,0 +1,21 @@
+const products = require("../products.json")
+
+
+
+
+const getProducts = (req, res) =>{
+    if(req.query.price){
+        const items = products.filter(element => element.price >= parseInt(req.query.price));
+        return res.status(200).send(items)
+    }
+    res.status(200).send(products);
+}
+
+
+
+
+
+
+
+
+module.exports = getProducts;
